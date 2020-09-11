215112c540048bfaa2f4df139843905cf351b8fb
andrei <andrew.m.kot@gmail.com>
2020-09-11 20:30:36 +0300

    some stuf

--- test_b/B.py
+++ test_b/B.py
@@ -1,6 +1,7 @@
-balance = int(input())
-price = list(map(int, input().split()))  # [21, 34, 18, 435, 18, 34, 20, 21, 19]
+balance = 100  # int(input())
+price = [21, 34, 18, 435, 18, 34, 20, 21, 19]  # list(map(int, input().split()))
 digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+max_digit = 9
 
 tmp = digits[0]
 
@@ -21,14 +22,13 @@ number_runk = balance // min_price
 if number_runk == 0:
     print(-1)
 else:
-    remainder = balance % min_price + min_price
+    remainder = balance % min_price
     max_number = [tmp] * number_runk
     n = 0
-    old_tmp = tmp
-    old_min_price = min_price
-    while remainder > 0 and n < old_tmp:
-        while tmp < 9:
-            if remainder >= price[tmp]:
+    while remainder > 0 and n < number_runk:
+        shift = 0
+        while tmp < max_digit:
+            if remainder >= price[tmp] + price[tmp]:
                 remainder = remainder + min_price - price[tmp]
                 min_price = price[tmp]
                 max_number[n] = digits[tmp]
